ced5b655a7ab125ab205d05a9e76a83b03b4cdf6
Austin M. Matherne <Austin.Matherne@Workiva.com>
2026-05-04 15:29:03 -0400

    Use common boto clients for script test downloads

--- tests/integration_tests/scripts/tests/webserver_validate_esef.py
+++ tests/integration_tests/scripts/tests/webserver_validate_esef.py
@@ -2,13 +2,12 @@ from __future__ import annotations
 
 import os
 import urllib.parse
-import urllib.request
 from pathlib import Path
 
 import regex
 import requests
 
-from tests.integration_tests.integration_test_util import get_s3_uri
+from tests.integration_tests.integration_test_util import download_from_public_s3
 from tests.integration_tests.scripts.script_util import (
     assert_result,
     parse_args,
@@ -32,13 +31,12 @@ working_directory = Path(args.working_directory)
 test_directory = Path(args.test_directory)
 report_zip_path = test_directory / 'TC2_invalid.zip'
 arelle_log_file = prepare_logfile(test_directory, this_file)
-report_zip_url = get_s3_uri(
-    'ci/packages/python_api_validate_esef.zip',
-    version_id='U3sEz.B8kjUWw0l6momz87EndK05cxFZ'
-)
-
 print(f"Downloading report: {report_zip_path}")
-urllib.request.urlretrieve(report_zip_url, report_zip_path)
+download_from_public_s3(
+    report_zip_path,
+    "ci/packages/python_api_validate_esef.zip",
+    version_id="U3sEz.B8kjUWw0l6momz87EndK05cxFZ",
+)
 
 print("Downloading packages...")
 package_assets = {
